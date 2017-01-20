e14269d88409b93f9db1f0b0f19c7fb0fbe74365
Dalton Childers <dalton.childers@ssrc.msstate.edu>
2017-01-20 01:34:20 -0600

    Updated Webpack Config
    Resolved bug reported in Issue #3. (https://github.com/hawkins/Game-Design-Assignment-1/issues/3)
    
    The .gitignore has been updated to include OS specific files.
    
    Signed-off-by: Dalton Childers <dalton.childers@ssrc.msstate.edu>

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path'); // eslint-disable-line import/no-unresolved
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
     app: ['./src/index.js']
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build/assets/'),
     publicPath: '/assets/',
     filename: 'bundle.js'
   },
