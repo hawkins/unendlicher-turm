e624dfd577058928f3df6393edd61f8eabb88b8f
Dalton Childers <dalton.childers@ssrc.msstate.edu>
2017-01-30 20:43:25 -0600

    Pixel Speech Bubbles Created and Added
    Signed-off-by: Dalton Childers <dalton.childers@ssrc.msstate.edu>

--- src/states/mainMenu.js
+++ src/states/mainMenu.js
@@ -13,6 +13,15 @@ function preload() {
   // Load title image file
   this.game.load.image('Title', 'assets/images/title.png');
 
+  // Load start game image file
+  this.game.load.image('startGame', 'assets/images/startGame.png');
+
+  // Load "How to Play" game image file
+  this.game.load.image('htpGame', 'assets/images/htpGame.png');
+
+  // Load Story game image file
+  this.game.load.image('storyGame', 'assets/images/storyGame.png');
+
   mainMenu.preload(this.game);
 }
 
@@ -20,7 +29,17 @@ function create() {
   // Create the map(s)
   mainMenu.create(this.game);
 
+  // Create Title Sprite
   this.game.add.sprite(30, 40, 'Title');
+
+  // Create Start Game Sprite
+  this.game.add.sprite(660, 335, 'startGame');
+
+  // Create "How to Play" Game Sprite
+  this.game.add.sprite(570, 35, 'htpGame');
+
+  // Create Story Game Sprite
+  this.game.add.sprite(565, 180, 'storyGame');
 
   // Create keys
   enter = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
