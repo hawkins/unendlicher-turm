ca865669257c798fc09c6567d87ff172c0ea7e88
Josh Hawkins <jkhaccounts@gmail.com>
2017-02-05 11:14:14 -0600

    Hotfix player spawn in town
    Just noticed it was off a bit so I moved it to the center of the little
    dirt patch. Oh well

--- src/states/town.js
+++ src/states/town.js
@@ -20,7 +20,7 @@ function preload() {
   // Load audio file
   this.game.load.audio('mainBackground', [ 'assets/audio/SoundEffects/adventure.ogg' ]);
 
-  playerController = new Player(this.game, 3, 15);
+  playerController = new Player(this.game, 4, 15);
   playerController.preload();
   town.preload(this.game);
 }
